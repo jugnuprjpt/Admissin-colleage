a0140d1dc6943469bf55af2b791644bf32368979
jugnu prajapati <jugnuprjpt@gmail.com>
2024-04-08 09:19:29 +0530

    demo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import BasicInformation from './Component/BasicInforation/BasicInformation';
+import DocumentUpload from './Component/DocumentUpload/DocumentUpload';
+import EducationDetail from './Component/EducationDetail/EducationDetail';
+import SubjectSelect from './Component/SubjectSlect/SubjectSelect';
+
+function App() {
+  return (
+    <>
+  
+  {/* <BasicInformation/> */}
+   <EducationDetail/> 
+  {/* <SubjectSelect/>
+  <DocumentUpload/> */}
+
+    </>
+  );
+}
+
+export default App;
